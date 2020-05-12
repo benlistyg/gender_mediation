f8b87ba409929587c13f421dd17e29c0c6cee451
Benjamin Listyg <listyg.ben@gmail.com>
2020-05-12 13:54:42 -0400

    Update code.R

--- code.R
+++ code.R
@@ -1,6 +1,7 @@
 library(mirt)
 library(lavaan)
 library(stringr)
+library(dplyr)
 
 # Importing Data from Github
 HCP <- data.table::fread('https://raw.githubusercontent.com/benlistyg/gender_mediation/master/HCP%20gender%20data.csv')
@@ -14,6 +15,7 @@ HCP$FS_L_WM_Hypointens_Vol <- NULL
 # Subsetting Narcissm items that don't require reverse scoring.
 # Adding back SubjectID, FamilyID, and Gender to dataset
   # Will join on these variables further down.
+# Items 6, 11, 21, 26, 36, 41, 51, 56 don't need reverse scoring
 N <- HCP %>% 
   select(NEORAW_06,
          NEORAW_11,
@@ -39,6 +41,7 @@ N <- HCP %>%
 # Subsetting Narcissm items that DO require reverse scoring (see Rollock and Lui (2015) for reference of items)
 # Adding back SubjectID, FamilyID, and Gender to dataset
 # Will join on these variables further down.
+# Items 1, 16, 31, and 46 need reverse scoring
 Reversed_N <- HCP %>% 
   select(NEORAW_01,
          NEORAW_16,
@@ -64,6 +67,9 @@ Reversed_N <- HCP %>%
 full_N <- merge(N, Reversed_N, by = c('Subject','Family_ID','Gender'))
 full_N$rowsum <- full_N[,-c(1:3)] %>% rowSums(., na.rm = T)
 
+        
+# Extraversion
+# Items 2, 7, 17, 22, 32, 37, 47, 52 don't need reverse scoring
 E <- HCP %>% 
   select(NEORAW_02,
          NEORAW_07,
@@ -86,6 +92,7 @@ E <- HCP %>%
         .) %>% 
   .[complete.cases(.),]
 
+# Items 12, 27, 42, and 57 need to be reverse scored
 Reversed_E <- HCP %>% 
   select(NEORAW_12,
          NEORAW_27,
@@ -107,6 +114,8 @@ Reversed_E <- HCP %>%
 full_E <- merge(E, Reversed_E, by = c('Subject','Family_ID','Gender'))
 full_E$rowsum <- full_E[,-c(1:3)] %>% rowSums(., na.rm = T)
 
+# Openness to Experience
+# Items 3, 8, 18, 23, 33, 38, 48 need reverse scoring
 Reversed_O <- HCP %>% 
   select(NEORAW_03,
          NEORAW_08,
@@ -128,6 +137,7 @@ Reversed_O <- HCP %>%
         .) %>% 
   .[complete.cases(.),]
 
+# Items 13, 28, 43, 53, 58 don't need reverse scoring
 O <- HCP %>% 
   select(NEORAW_13,
          NEORAW_28,
@@ -150,6 +160,8 @@ O <- HCP %>%
 full_O <- merge(O, Reversed_O, by = c('Subject','Family_ID','Gender'))
 full_O$rowsum <- full_O[,-c(1:3)] %>% rowSums(., na.rm = T)
 
+# Agreeableness
+# Items 9, 14, 24, 29, 39, 44, 54, 59 need reverse scoring
 Reversed_A <- HCP %>% 
   select(NEORAW_09,
          NEORAW_14,
@@ -172,6 +184,7 @@ Reversed_A <- HCP %>%
         .) %>% 
   .[complete.cases(.),]
 
+# Items 4, 19, 39, 49 don't need reverse scoring
 A <- HCP %>% 
   select(NEORAW_04,
          NEORAW_19,
@@ -193,6 +206,8 @@ A <- HCP %>%
 full_A <- merge(A, Reversed_A, by = c('Subject','Family_ID','Gender'))
 full_A$rowsum <- full_A[,-c(1:3)] %>% rowSums(., na.rm = T)
 
+# Conscientiousness
+# Items 5, 10, 20, 25, 35, 40, 50, 60 don't need reverse scoring
 C <- HCP %>% 
   select(NEORAW_05,
          NEORAW_10,
@@ -215,6 +230,7 @@ C <- HCP %>%
         .) %>% 
   .[complete.cases(.),]
 
+# Items 15, 30, 45, 55 need to be reverse scored
 Reversed_C <- HCP %>% 
   select(NEORAW_15,
          NEORAW_30,
@@ -252,6 +268,7 @@ C_model <- mirt(full_C[,-c(1:3,ncol(full_C))],1,technical = list(removeEmptyRows
 #####
 
 # Combining Subject ID, FamilyID, Gender, and IRT Thetas into long dataframe.
+        # `fscores` is from the MIRT package and returns IRT theta values from a fitted IRT model
 
 B5_data <- rbind(
   data.frame(
@@ -289,7 +306,8 @@ B5_data <- rbind(
     Gender = C$Gender,
     B5 = 'C'))
 
-# Creating long dataframe of brain data
+# Creating long dataframe of brain data (based on the variables that have "Thck", "Vol" or "Area" in their name
+        # 260 in total
 # Joining brain data with B5 data on subjectid and family ID
 
 Brain_data <- HCP %>% 
@@ -308,7 +326,12 @@ full_data <- merge(B5_data,
 #####
 
 # Function to iterate over long dataframe
-
+# This function takes in a Big 5 Trait and Brain Area
+# Subsets the long dataframe (full_data)
+# Fits a multilevel mediation model
+        # Family ID is the cluster
+# Returns parameter estimates for the fitted model
+# Takes around 15 minutes to run over all 1300 combinations of B5 x Brain Region
 model_function <- function(personality,
                            brain_area,
                            ...){
@@ -319,12 +342,12 @@ model_function <- function(personality,
     filter(B5 == personality,
            Brain_Area == brain_area) %>% 
     mutate(Gender = recode(Gender, 
-                           'M' = 0,
-                           'F' = 1))
+                           'M' = 0, # Recoding males as a 0
+                           'F' = 1))# Recoding females as a 1
   
   Data <- data.frame(X = in_function_data$Gender, 
-                     Y = scale(in_function_data$F1), 
-                     M = scale(in_function_data$Brain_Value),
+                     Y = scale(in_function_data$F1), # Z-scoring the IRT thetas, interpret this as SD increase / decrease
+                     M = scale(in_function_data$Brain_Value), #Z-scoring the brain region, interpret this as SD increase / decrease
                      Family_ID = in_function_data$Family_ID)
   
   model <- 'level: 1
@@ -349,11 +372,13 @@ model_function <- function(personality,
            # total effect
              total := c + (a*b)
          '
+  # Using robust maximum likelihood for estimation
   fit <- sem(model, 
              data = Data,
              cluster = 'Family_ID',
              estimator = 'MLR')
   
+  #Adding back the brain region and personality trait to the parameter estimates dataframe
   cbind(
     parameterestimates(fit, standardized = T),
     personality,
@@ -364,10 +389,15 @@ model_function <- function(personality,
 
 #####
 
+#Conditions is a dataframe with all possible combinations of B5 and Brain Region
+        # This is what model_function iterates over
 conditions <- expand.grid(unique(full_data$B5),
                           unique(full_data$Brain_Area)) %>% 
   `colnames<-`(c('personality',"brain_area"))
 
+# This is the part that does the row-wise iteration
+        # final_data contains all the lavaan parameter estimates from the 1300 combinations
+        
 final_data <- plyr::mdply(.data = conditions, 
                           .fun = model_function)
 
